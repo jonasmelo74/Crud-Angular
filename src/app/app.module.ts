d254ac866d0f9bac32eb9465959a62740c84a867
Jonas Melo <jonasmelodapaz74@gmail.com>
2023-01-16 19:55:11 -0300

    criando modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { CursosModule } from './cursos/cursos.module';
 import { NgModule } from '@angular/core';
+// Prepara a aplicação para ser executada em um browser
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // Serão listados todos os componentes diretivas e pipes
   declarations: [
     AppComponent
   ],
+  // Outros módulos que deveram ser executados
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
-  providers: [],
+  providers: [], // Serviçõs que ficaram disponiveis para todos os componentes declarados nesse modulo
   bootstrap: [AppComponent]
 })
+ 
 export class AppModule { }
---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,17 @@
+import { CursosComponent } from './cursos.component copy';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+  CursosComponent   
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CursosComponent
+  ]
+})
+   
+export class CursosModule { }
